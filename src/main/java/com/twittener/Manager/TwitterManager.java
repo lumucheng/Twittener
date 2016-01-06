c26ab96fe8d578ba5ff13e321a432239bb5ee8ce
Mucheng <lumucheng@gmail.com>
2016-01-06 21:00:46 +0800

    Added StanfordNLP libraries.
    WIP: Trying out StanfordNLP to give sentiment values to tweets.

--- src/main/java/com/twittener/Manager/TwitterManager.java
+++ src/main/java/com/twittener/Manager/TwitterManager.java
@@ -100,50 +100,56 @@ public class TwitterManager {
                 result = twitter.search(query);
                 List<Status> tweets = result.getTweets();
 
+                
+                NLPManager.init();
+                
                 for (final Status tweet : tweets) {
 
                     if (tweet.getInReplyToScreenName() == null) {
 
                         count++;
-                        tweetDAO.insertTweet(tweet);
+                        
+                        System.out.println(tweet.getText() + " : " + NLPManager.findSentiment(tweet.getText()));
+                        
+//                        tweetDAO.insertTweet(tweet);
 
-                        MediaEntity[] mediaArray = tweet.getMediaEntities();
-                        for (MediaEntity media : mediaArray) {
-                            TweetMedia tweetMedia = new TweetMedia();
-                            tweetMedia.setMedia_Id(media.getId());
-                            tweetMedia.setMedia_Type(media.getType());
-                            tweetMedia.setMedia_Url(media.getMediaURL());
-                            tweetMedia.setTweet_Id(tweet.getId());
-                            tweetMedia.setMedia_Caption("@" + tweet.getUser().getName()
-                            + ":" + tweet.getText());
-                            tweetMediaDAO.insertTweetMedia(tweetMedia);
-                        }
-
-                        ExtendedMediaEntity[] extMediaArray = tweet.getExtendedMediaEntities();
-                        for (ExtendedMediaEntity extended : extMediaArray) {
-                            if (extended.getType().equals("video")) {
-                                TweetMedia videoMedia = new TweetMedia();
-                                videoMedia.setMedia_Id(extended.getId());
-                                videoMedia.setMedia_Type(extended.getType());
-                                videoMedia.setMedia_Url(extended.getMediaURL());
-                                videoMedia.setTweet_Id(tweet.getId());
-                                videoMedia.setMedia_Caption("@" + tweet.getUser().getName()
-                                        + ":" + tweet.getText());
-                                videoMedia.setMedia_VideoUrl(extended.getVideoVariants()[2].getUrl());
-                                tweetMediaDAO.insertTweetMedia(videoMedia);
-                            }
-                        }
-
-                        /* Generate wav file on another thread 
-                        in order to prevent blocking main thread */
-                        new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                final String formattedTweet = tweet.getText().replaceAll("\\S+://\\S+", "");
-                                ttsMgr.convert(formattedTweet, tweet.getId());
-                            }
-
-                        }).start();
+//                        MediaEntity[] mediaArray = tweet.getMediaEntities();
+//                        for (MediaEntity media : mediaArray) {
+//                            TweetMedia tweetMedia = new TweetMedia();
+//                            tweetMedia.setMedia_Id(media.getId());
+//                            tweetMedia.setMedia_Type(media.getType());
+//                            tweetMedia.setMedia_Url(media.getMediaURL());
+//                            tweetMedia.setTweet_Id(tweet.getId());
+//                            tweetMedia.setMedia_Caption("@" + tweet.getUser().getName()
+//                            + ":" + tweet.getText());
+//                            tweetMediaDAO.insertTweetMedia(tweetMedia);
+//                        }
+//
+//                        ExtendedMediaEntity[] extMediaArray = tweet.getExtendedMediaEntities();
+//                        for (ExtendedMediaEntity extended : extMediaArray) {
+//                            if (extended.getType().equals("video")) {
+//                                TweetMedia videoMedia = new TweetMedia();
+//                                videoMedia.setMedia_Id(extended.getId());
+//                                videoMedia.setMedia_Type(extended.getType());
+//                                videoMedia.setMedia_Url(extended.getMediaURL());
+//                                videoMedia.setTweet_Id(tweet.getId());
+//                                videoMedia.setMedia_Caption("@" + tweet.getUser().getName()
+//                                        + ":" + tweet.getText());
+//                                videoMedia.setMedia_VideoUrl(extended.getVideoVariants()[2].getUrl());
+//                                tweetMediaDAO.insertTweetMedia(videoMedia);
+//                            }
+//                        }
+//
+//                        /* Generate wav file on another thread 
+//                        in order to prevent blocking main thread */
+//                        new Thread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                final String formattedTweet = tweet.getText().replaceAll("\\S+://\\S+", "");
+//                                ttsMgr.convert(formattedTweet, tweet.getId());
+//                            }
+//
+//                        }).start();
                     }
                 }
             } while ((query = result.nextQuery()) != null && count < 60);
