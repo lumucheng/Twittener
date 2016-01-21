322579719f8e87897e4e94c50868f18a8ff5e4fa
Mucheng <lumucheng@gmail.com>
2016-01-21 17:16:45 +0800

    Bug fix.

--- src/main/java/com/twittener/DAO/TopicAccountsDAO.java
+++ src/main/java/com/twittener/DAO/TopicAccountsDAO.java
@@ -8,11 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import twitter4j.Status;
 
 public class TopicAccountsDAO {
     
